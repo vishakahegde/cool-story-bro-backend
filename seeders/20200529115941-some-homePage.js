1856f6287c021d610ef4a05b234a9d5e6b4f13a2
vishakahegde <hegde.vishaka@gmail.com>
2020-05-29 17:29:27 +0200

    delete default values from seed file for homepage

--- seeders/20200529115941-some-homePage.js
+++ seeders/20200529115941-some-homePage.js
@@ -8,8 +8,6 @@ module.exports = {
         {
           title: "Born & Raised in Philly",
           description: "How I became the prince of Bel Air",
-          backgroundColor: "#ffffff",
-          color: "#000000",
           createdAt: new Date(),
           updatedAt: new Date(),
           userId: 1,
@@ -17,8 +15,6 @@ module.exports = {
         {
           title: "How I became a programmer",
           description: "How I learnt to stop worrying and love the typos",
-          backgroundColor: "#ffffff",
-          color: "#000000",
           createdAt: new Date(),
           updatedAt: new Date(),
           userId: 2,
