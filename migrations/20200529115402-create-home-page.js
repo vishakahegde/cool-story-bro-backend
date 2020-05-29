d1a02054fae460490835cb67e4638608cbc7053b
vishakahegde <hegde.vishaka@gmail.com>
2020-05-29 17:05:58 +0200

    update for default values

--- migrations/20200529115402-create-home-page.js
+++ migrations/20200529115402-create-home-page.js
@@ -17,9 +17,11 @@ module.exports = {
       },
       backgroundColor: {
         type: Sequelize.STRING,
+        defaultValue: "#ffffff",
       },
       color: {
         type: Sequelize.STRING,
+        defaultValue: "#000000",
       },
       createdAt: {
         allowNull: false,
